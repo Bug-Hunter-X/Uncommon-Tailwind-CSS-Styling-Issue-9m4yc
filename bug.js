aa4d5eb9502d28f4f1fcdc28e45d94006bc59720
BugHunter-X <askzenai@gmail.com>
2025-02-06 12:36:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,8 @@
+```javascript
+// ... your Tailwind CSS classes
+<div class="bg-gray-100 p-4">
+  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    Click Me
+  </button>
+</div>
+```
